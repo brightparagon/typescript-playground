74f35351893cfca35dae8275a71947ab0fa8a933
노경모 <kyeongmo2@gmail.com>
2018-10-03 21:33:39 +0900

    Type: function, object, type alias

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 "use strict";
+// string type
 var a = 'sgg';
 // a = 22 // error
+// number type
 var b = 28;
 // b = 'wow' // error
+// boolean type
 var c = true;
 // c = 1 // error
+// any type
 var d; // type: any
 d = 'abc'; // don't get any error from tsc
 d = 21;
-var e;
-e = 22;
-// e = 'ss' // error
+// array type
 var arr = ['aa', 'bb'];
 // let arr: any[] = ['aa', 'bb']
 // arr = [100] // error
+// tuple type
 var tuple = ['abc', 1000];
 // let tuple: [string, number] = [1000, 'abc'] // error
+// enum type
 var Color;
 (function (Color) {
     Color[Color["Gray"] = 0] = "Gray";
@@ -23,4 +27,46 @@ var Color;
     Color[Color["Blue"] = 101] = "Blue"; // 101
 })(Color || (Color = {}));
 var myColor = Color.Blue;
-console.log(myColor);
+// console.log(myColor);
+// function type
+function test() {
+    return 'abc';
+    // return 2 // error
+}
+function test2() {
+    console.log('?');
+    // return 2 // error
+}
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+// console.log(multiply(1, 2));
+var myMultiply;
+myMultiply = multiply;
+// myMultiply = test2 // error
+// object type
+var user = {
+    name: 'jeffrey',
+    age: 28
+};
+// user = {} // error
+// user = {
+//   name: '?',
+//   age: '20' // error
+// }
+// user = {
+//   name: '?',
+//   age: 20 // ok
+// }
+var complex = {
+    data: [1, 2, 3],
+    output: function (all) {
+        return [2, 3];
+    }
+};
+var complex2 = {
+    data: [1, 2, 3],
+    output: function (all) {
+        return '';
+    }
+};
--- app.ts
+++ app.ts
@@ -1,27 +1,30 @@
+// string type
 let a: string = 'sgg'
 // a = 22 // error
 
+// number type
 let b: number = 28
 // b = 'wow' // error
 
+// boolean type
 let c: boolean = true
 // c = 1 // error
 
+// any type
 let d // type: any
 d = 'abc' // don't get any error from tsc
 d = 21
 
-let e: number
-e = 22
-// e = 'ss' // error
-
+// array type
 let arr: string[] = ['aa', 'bb']
 // let arr: any[] = ['aa', 'bb']
 // arr = [100] // error
 
+// tuple type
 let tuple: [string, number] = ['abc', 1000]
 // let tuple: [string, number] = [1000, 'abc'] // error
 
+// enum type
 enum Color {
   Gray, // 0
   Green = 100, // 100
@@ -29,4 +32,57 @@ enum Color {
 }
 
 let myColor: Color = Color.Blue
-console.log(myColor);
+// console.log(myColor);
+
+// function type
+function test(): string {
+  return 'abc'
+  // return 2 // error
+}
+
+function test2(): void {
+  console.log('?')
+  // return 2 // error
+}
+
+function multiply(val1: number, val2: number): number {
+  return val1 * val2
+}
+// console.log(multiply(1, 2));
+
+let myMultiply: (val: number, val2: number) => number
+myMultiply = multiply
+// myMultiply = test2 // error
+
+// object type
+let user: { name: string, age: number } = {
+  name: 'jeffrey',
+  age: 28
+}
+// user = {} // error
+// user = {
+//   name: '?',
+//   age: '20' // error
+// }
+// user = {
+//   name: '?',
+//   age: 20 // ok
+// }
+
+let complex: { data: number[], output: (all: boolean) => number[] } = {
+  data: [1, 2, 3],
+  output: (all: boolean) => {
+    return [2, 3]
+  }
+}
+// complex = {} // error
+
+// type alias
+type Complex = { data: number[], output: (all: boolean) => number[] };
+
+let complex2: Complex = {
+  data: [1, 2, 3],
+  output: (all: boolean) => {
+    return [2, 3]
+  }
+}
