623cce2c6ff8169618b297dd497b5f1191b47d15
노경모 <kyeongmo2@gmail.com>
2018-10-03 19:58:55 +0900

    Type: array, tuple, enum

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 "use strict";
-var ss = 'sgg';
-// ss = 22
+var a = 'sgg';
+// a = 22 // error
+var b = 28;
+// b = 'wow' // error
+var c = true;
+// c = 1 // error
+var d; // type: any
+d = 'abc'; // don't get any error from tsc
+d = 21;
+var e;
+e = 22;
+// e = 'ss' // error
+var arr = ['aa', 'bb'];
+// let arr: any[] = ['aa', 'bb']
+// arr = [100] // error
+var tuple = ['abc', 1000];
+// let tuple: [string, number] = [1000, 'abc'] // error
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 101
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -15,4 +15,18 @@ let e: number
 e = 22
 // e = 'ss' // error
 
+let arr: string[] = ['aa', 'bb']
+// let arr: any[] = ['aa', 'bb']
+// arr = [100] // error
 
+let tuple: [string, number] = ['abc', 1000]
+// let tuple: [string, number] = [1000, 'abc'] // error
+
+enum Color {
+  Gray, // 0
+  Green = 100, // 100
+  Blue // 101
+}
+
+let myColor: Color = Color.Blue
+console.log(myColor);
