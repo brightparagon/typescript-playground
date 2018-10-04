f5416e9ab8f92c3e64aa542fb756d00f462d351e
노경모 <kyeongmo2@gmail.com>
2018-10-04 23:37:16 +0900

    Exercise: complex object

---
+++ exercise.ts
@@ -0,0 +1,21 @@
+type BankAccount = {
+  money: number,
+  deposit: (value: number) => void
+}
+
+let bankAccount: BankAccount = {
+  money: 2000,
+  deposit(value: number): void {
+    this.money += value
+  }
+}
+
+let mySelf: { name: string, bankAccount: BankAccount, hobbies: string[] } = {
+  name: 'Jeffrey',
+  bankAccount: bankAccount,
+  hobbies: ['Sports', 'Cooking']
+}
+
+mySelf.bankAccount.deposit(3000)
+
+console.log(mySelf);
