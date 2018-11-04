20234628fae5e4182de4a1ae381315b4a0c3eda2
노경모 <kyeongmo2@gmail.com>
2018-11-04 15:20:31 +0900

    exercise: generic class

--- generic/app.js
+++ generic/app.js
@@ -32,3 +32,54 @@ printAll(["aaa", "bbb", "ccc"]);
 // Generic Types
 var echo2 = betterEcho;
 console.log(echo2("oooops!"));
+// Generic Class with constraints
+// class SimpleMath<T extends number | string, U extends number | string> {
+//   baseValue: T
+//   multiplyValue: U
+//   calculate(): number {
+//     return +this.baseValue * +this.multiplyValue
+//   }
+// }
+// const simpleMath = new SimpleMath<string, number>()
+// simpleMath.baseValue = "10"
+// simpleMath.multiplyValue = 20
+// console.log(simpleMath.calculate())
+// Exercise
+var MyMap = /** @class */ (function () {
+    function MyMap() {
+        this.obj = {};
+    }
+    MyMap.prototype.setItem = function (key, item) {
+        this.obj[key] = item;
+    };
+    MyMap.prototype.getItem = function (key) {
+        return this.obj[key];
+    };
+    MyMap.prototype.clear = function () {
+        var _this = this;
+        Object
+            .keys(this.obj)
+            .forEach(function (key) { return delete _this.obj[key]; });
+    };
+    MyMap.prototype.printMap = function () {
+        var _this = this;
+        Object
+            .keys(this.obj)
+            .forEach(function (key) { return console.log(_this.obj[key]); });
+    };
+    return MyMap;
+}());
+var numberMap = new MyMap();
+numberMap.setItem('apples', 555);
+numberMap.setItem('bananas', 10000055);
+console.log(numberMap.getItem('bananas')); // 10000055
+console.log(numberMap.getItem('apples')); // 555
+numberMap.clear();
+console.log(numberMap.getItem('bananas')); // undefined
+numberMap.setItem('apples', 444);
+numberMap.setItem('bananas', 1234);
+numberMap.printMap(); // 444, 1234
+var stringMap = new MyMap();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap(); // "Max", 27
--- generic/app.ts
+++ generic/app.ts
@@ -41,16 +41,59 @@ const echo2: <T>(data: T) => T = betterEcho
 console.log(echo2<string>("oooops!"))
 
 // Generic Class with constraints
-class SimpleMath<T extends number | string, U extends number | string> {
-  baseValue: T
-  multiplyValue: U
+// class SimpleMath<T extends number | string, U extends number | string> {
+//   baseValue: T
+//   multiplyValue: U
   
-  calculate(): number {
-    return +this.baseValue * +this.multiplyValue
+//   calculate(): number {
+//     return +this.baseValue * +this.multiplyValue
+//   }
+// }
+
+// const simpleMath = new SimpleMath<string, number>()
+// simpleMath.baseValue = "10"
+// simpleMath.multiplyValue = 20
+// console.log(simpleMath.calculate())
+
+// Exercise: Generic Class
+class MyMap<T extends number | string> {
+  obj: {
+    [key: string]: T
+  } = {}
+
+  setItem(key: string, item: T): void {
+    this.obj[key] = item
+  }
+
+  getItem(key: string): T {
+    return this.obj[key]
+  }
+
+  clear(): void {
+    Object
+      .keys(this.obj)
+      .forEach((key) => delete this.obj[key])
+  }
+
+  printMap(): void {
+    Object
+      .keys(this.obj)
+      .forEach((key) => console.log(this.obj[key]))
   }
 }
 
-const simpleMath = new SimpleMath<string, number>()
-simpleMath.baseValue = "10"
-simpleMath.multiplyValue = 20
-console.log(simpleMath.calculate())
+const numberMap = new MyMap<number>();
+numberMap.setItem('apples', 555);
+numberMap.setItem('bananas', 10000055);
+console.log(numberMap.getItem('bananas')) // 10000055
+console.log(numberMap.getItem('apples')) // 555
+numberMap.clear()
+console.log(numberMap.getItem('bananas')) // undefined
+numberMap.setItem('apples', 444);
+numberMap.setItem('bananas', 1234);
+numberMap.printMap(); // 444, 1234
+ 
+const stringMap = new MyMap<string>();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap(); // "Max", 27
