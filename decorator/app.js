beebf78d4470be0079eae37dcd0eb1c64a46bfa2
노경모 <kyeongmo2@gmail.com>
2018-11-16 00:03:28 +0900

    decorator: parameter decorator

--- decorator/app.js
+++ decorator/app.js
@@ -1,9 +1,11 @@
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
 };
 // Decorator
 function withLogging(constructorFn) {
@@ -47,4 +49,66 @@ var Plant = /** @class */ (function () {
     return Plant;
 }());
 var plant = new Plant();
-plant.print();
+// plant.print()
+// Method Decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        // descriptor.writable = value
+    };
+}
+// Property Decorator
+function overwritable(value) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+var Project2 = /** @class */ (function () {
+    function Project2(name) {
+        this.calcBudget = function () {
+            console.log(1000);
+        };
+        this.projectName = name;
+    }
+    __decorate([
+        overwritable(false)
+    ], Project2.prototype, "projectName");
+    __decorate([
+        editable(false)
+    ], Project2.prototype, "calcBudget");
+    return Project2;
+}());
+var project = new Project2("New Project");
+project.calcBudget();
+project.calcBudget = function () {
+    console.log(2000);
+};
+project.calcBudget();
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log(target);
+    console.log(methodName);
+    console.log(paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(mode + 10000);
+        }
+        else {
+            console.log(mode + 2000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers");
+    return Course;
+}());
+var course = new Course("Super Course");
+course.printStudentNumbers("anythig", true);
+course.printStudentNumbers("anythig", false);
--- decorator/app.ts
+++ decorator/app.ts
@@ -35,18 +35,21 @@ class Plant {
 }
 
 const plant = new Plant()
-plant.print()
+// plant.print()
 
 // Method Decorator
 function editable(value: boolean): any {
   return function(target: any, propName: string, descriptor: PropertyDecorator) {
-    descriptor.writable = value
+    // descriptor.writable = value
   }
 }
 
 // Property Decorator
 function overwritable(value: boolean): any {
   return function(target: any, propName: string) {
+    console.log(target)
+    console.log(propName)
+
     const newDescriptor: PropertyDescriptor = {
       writable: value
     }
@@ -55,7 +58,7 @@ function overwritable(value: boolean): any {
   }
 }
 
-class Project {
+class Project2 {
   @overwritable(false)
   projectName: string
 
@@ -69,9 +72,36 @@ class Project {
   }
 }
 
-const project = new Project("New Project")
+const project = new Project2("New Project")
 project.calcBudget()
 project.calcBudget = () => {
   console.log(2000)
 }
 project.calcBudget()
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+  console.log(target)
+  console.log(methodName)
+  console.log(paramIndex)
+}
+
+class Course {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+    if (printAll) {
+      console.log(mode + 10000)
+    } else {
+      console.log(mode + 2000)
+    }
+  }
+}
+
+const course = new Course("Super Course")
+course.printStudentNumbers("anythig", true)
+course.printStudentNumbers("anythig", false)
